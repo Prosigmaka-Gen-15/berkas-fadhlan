faf8c84d0dce848a263d712db059e44362859437
fadhlanfs <fadhlansal32@gmail.com>
2023-07-27 10:34:23 +0700

    react: update button

--- gen-23.05-react-basic/src/App.jsx
+++ gen-23.05-react-basic/src/App.jsx
@@ -125,15 +125,10 @@ export default function App() {
             {/* Tombol quantity */}
             <div className='flex space-x-2 mb-4'>
               <div className='flex items-center border border-gray-200 rounded'>
-                <button type='button' class='w-10 h-10 leading-10 text-gray-600 transition hover:opacity-75'>
+                <button type='button' className='w-10 h-10 leading-10 text-gray-600 transition hover:opacity-75'>
                   &minus;
                 </button>
-                <input
-                  type='button'
-                  id='Quantity'
-                  value='0'
-                  class='h-10 w-16 border-transparent text-center [-moz-appearance:_textfield] sm:text-sm [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none;'
-                />
+                <input type='button' id='Quantity' value='0' class='h-10 w-16 border-transparent text-center' />
                 <button type='button' className='w-10 h-10 leading-10 text-gray-600 transition hover:opacity-75'>
                   +
                 </button>
