d9bdbfb65f651afc08591455b1b8459422590096
fadhlanfs <fadhlansal32@gmail.com>
2023-07-30 12:17:15 +0700

    react state initial commit

---
+++ gen-23.05-react-state/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className='bg-gray-200 text-center py-4 h-full flex items-center justify-center'>
+      <p>&copy; 2023 Your Website. All rights reserved.</p>
+    </div>
+  );
+}
---
+++ gen-23.05-react-state/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+export default function Header(props) {
+  return (
+    <div className='nav-links bg-gray-100 gap-x-2 fixed top-0 w-full h-10 flex justify-between items-center'>
+      <div className='flex items-center'>
+        <a href='#' className='px-4 py-1 rounded-md text-gray-700 hover:text-white transition duration-200 hover:bg-black duration-200 ease-in'>
+          Home
+        </a>
+      </div>
+      <div className='flex items-center'>
+        <FontAwesomeIcon
+          icon={faSearch}
+          className='px-4 py-1 rounded-md text-gray-700 hover:text-white transition duration-200 hover:bg-black duration-200 ease-in cursor-pointer'
+        />
+        <FontAwesomeIcon
+          icon={faShoppingCart}
+          className='px-4 py-1 rounded-md text-gray-700 hover:text-white transition duration-200 hover:bg-black duration-200 ease-in cursor-pointer'
+        />
+        <a href='#' className='px-4 py-1 rounded-md text-gray-700 hover:text-white transition duration-200 hover:bg-black duration-200 ease-in'>
+          {props.loginText}
+        </a>
+        <a href='#' className='px-4 py-1 rounded-md text-gray-700 hover:text-white transition duration-200 hover:bg-black duration-200 ease-in'>
+          {props.registerText}
+        </a>
+      </div>
+    </div>
+  );
+}
